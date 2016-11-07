056e2a0c6925e9758f1d5f3ac1025eb802c417f5
Erica Nafziger and Lina Shadrach <lina@>
2016-11-07 11:18:12 -0800

    fill in sidebar with input from the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 //backend logic
-var Bio = function (avatarPhoto, fullName, nameArr, bioText, userName, city, state){
+var Bio = function (avatarURL, avatarPhoto, fullName, nameArr, bioText, userName, city, state){
+  this.avatarURL = avatarURL;
   this.avatarPhoto = avatarPhoto;
   this.fullName = fullName;
   this.nameArr = [];
@@ -11,6 +12,9 @@ Bio.prototype={
   constructor: Bio,
   getFullName: function(){
     this.fullName = this.nameArr[0] + " " + this.nameArr[1];
+  }
+  addImgTag: function(){
+    this.avatarPhoto = '<img src ="' + this.avatarURL + '" >';
   }
 };
 
@@ -29,10 +33,10 @@ var BlogEntry = function(entryTitle, photos, entryText, entryTags) {
 
 //frontend logic
 $(document).ready(function(){
+  var user = new User();
   $("#userRegister").submit(function(event){
     event.preventDefault();
 
-    var user = new User();
     user.bio.nameArr[0] = $(".first-name").val();
     user.bio.nameArr[1] = $(".last-name").val();
     user.bio.blogTitle = $(".blogTitle").val();
@@ -42,9 +46,17 @@ $(document).ready(function(){
     user.bio.city = $(".city").val();
     user.bio.state = $(".state").val();
     user.bio.getFullName();
+    user.bio.addImgTag();
     console.log(user);
     $("#landingPage").hide();
     $("#mainblog").show();
-  })
-
+  });
+  var showSidebarInput = function(){
+    $(".userName").text(user.bio.userName);
+    $(".avatarImg").text(user.bio.avatarPhoto);
+    $(".fullName").text(user.bio.fullName);
+    $(".location").text(user.bio.city + ", " + user.bio.state);
+    $(".bio").text(user.bio.bioText);
+    // $("#sidebarBlogList").prepend();
+  };
 });
