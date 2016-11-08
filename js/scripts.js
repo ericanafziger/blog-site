b66c5d5dd92afb3e592f0e3864f07b88efd92830
Erica Nafziger and Ezra Sandzer-Bell <ezra@>
2016-11-08 09:00:12 -0800

    update avatar img to resize and fit in circle and add character counter in short bio

--- js/scripts.js
+++ js/scripts.js
@@ -61,6 +61,18 @@ var addTags = function(tags) {
 
 //frontend logic
 $(document).ready(function(){
+
+    var text_max = 250;
+    $('#count_message').html(text_max + ' remaining');
+
+    $('#text').keyup(function() {
+      var text_length = $('#text').val().length;
+      var text_remaining = text_max - text_length;
+
+      $('#count_message').html(text_remaining + ' remaining');
+    });
+
+
   var user = new User();
 
   var showSidebarInput = function(){
